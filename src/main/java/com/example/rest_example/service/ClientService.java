07f7222f0fd4ce266a9cbd04dae381b899907a91
VelazqueZzz <m.italy@vk.com>
2024-01-29 23:05:31 +0300

    Initial commit

---
+++ src/main/java/com/example/rest_example/service/ClientService.java
@@ -0,0 +1,13 @@
+package com.example.rest_example.service;
+
+import com.example.rest_example.model.Client;
+
+import java.util.List;
+
+public interface ClientService {
+    void create(Client client);
+    List<Client> readAll();
+    Client read(int id);
+    boolean update(Client client,int id);
+    boolean delete(int id);
+}
